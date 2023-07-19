b92aef7fc02d12e1061a9404776e64a2c43e5ab4
Saggi Mizrahi <smizrahi@google.com>
2023-07-19 15:02:12 +0000

    Release workflow

---
+++ tools/mc2bq/aux/release-artifacts.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+cd "$(dirname "${BASH_SOURCE[0]}")/.."
+
+if [ -n "${GITHUB_REF}" ]; then
+  tag_name="${GITHUB_REF##*/}"
+else
+  tag_name=dev
+fi
+
+find . -name "mc2bq_${tag_name}_*.tar.gz" -or -name "mc2bq_${tag_name}_*.zip" | while read -r artifact; do
+  if [ "${tag_name}" == "dev" ]; then
+    echo "Dev version not releasing"
+    echo ${artifact}
+  else
+    hub release create "${artifact}" "${tag_name}"
+  fi
+done
