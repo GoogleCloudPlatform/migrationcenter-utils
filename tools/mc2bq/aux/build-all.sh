3660e23550a8c38afe64a4f5dd87f81b8657ba8a
Saggi Mizrahi <smizrahi@google.com>
2023-07-19 15:02:12 +0000

    Release workflow

---
+++ tools/mc2bq/aux/build-all.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -e
+if [ -n "${GITHUB_REF}" ]; then
+  tag_name="${GITHUB_REF##*/}"
+else
+  tag_name=dev
+fi
+
+TARGETS=(
+  windows,amd64,.exe,zip
+  windows,arm,.exe,zip
+  linux,amd64,,tar.gz
+  linux,arm,,tar.gz
+  linux,arm64,,tar.gz
+  darwin,amd64,,tar.gz
+  darwin,arm64,,tar.gz
+)
+
+cd "$(dirname "${BASH_SOURCE[0]}")/.."
+env
+
+for target in "${TARGETS[@]}"; do
+  IFS=, read os arch suffix package <<<$target
+  dir=dist/${os}_${arch}
+  GOOS=$os GOARCH=$arch go build -o "$dir/mc2bq${suffix}"
+  pushd $dir
+  case "${package}" in
+    zip)
+      fname=mc2bq_${tag_name}_${os}_${arch}.zip
+      zip "${fname}" *
+    ;;
+    tar*)
+      fname=mc2bq_${tag_name}_${os}_${arch}.${package}
+      tar -cf "${fname}" *
+    ;;
+    *)
+    exit 1
+    ;;
+  esac
+  hub release create "${fname}" -m "$tag_name" "$tag_name"
+  popd
+done
