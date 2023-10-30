bf217ec94abce95f29391c7a56d1fb967c41a250
Saggi Mizrahi <smizrahi@google.com>
2023-10-30 14:56:10 +0000

    Use protobuf reflection for serialization
    This is used instead of regular Go relection because it is harder
    to map it to the table schema. The protobuf is the canonical
    representation making converison easier.

--- tools/mc2bq/pkg/schema/schema_test.go
+++ tools/mc2bq/pkg/schema/schema_test.go
@@ -32,107 +32,49 @@ import (
 // The tests uses a golden output file.
 // To update it run: go test -test.generate-golden-files $PWD
 func TestObjectSerializer(t *testing.T) {
-	type TestInnerStruct struct {
-		FieldA string
-		FieldB int
-	}
-	innerStructSchema := bigquery.Schema{
-		{Name: "field_a", Type: bigquery.StringFieldType},
-		{Name: "field_b", Type: bigquery.IntegerFieldType},
-	}
-	type TestStruct struct {
-		IntScalar       int
-		StringScalar    string
-		BoolScalar      bool
-		Float32Scalar   float32
-		Float64Scalar   float64
-		IntArray        []int
-		Timestamp       timestamppb.Timestamp
-		TimestampPtr    *timestamppb.Timestamp
-		DynamicMap      map[string]string
-		Record          TestInnerStruct
-		RecordPtr       *TestInnerStruct
-		NullPtr         *TestInnerStruct
-		RecordArray     []*TestInnerStruct
-		RecordMap       map[string]*TestInnerStruct
-		DeprecatedField string
-		Enum            migrationcenterpb.CommitmentPlan
-	}
-	schema := bigquery.Schema{
-		{Name: "int_scalar", Type: bigquery.IntegerFieldType},
-		{Name: "string_scalar", Type: bigquery.StringFieldType},
-		{Name: "bool_scalar", Type: bigquery.BooleanFieldType},
-		{Name: "float32_scalar", Type: bigquery.FloatFieldType},
-		{Name: "float64_scalar", Type: bigquery.FloatFieldType},
-		{Name: "int_array", Type: bigquery.IntegerFieldType, Repeated: true},
-		{Name: "timestamp", Type: bigquery.TimestampFieldType},
-		{Name: "timestamp_ptr", Type: bigquery.TimestampFieldType},
-		{Name: "dynamic_map", Type: bigquery.RecordFieldType, Repeated: true, Schema: bigquery.Schema{
-			{Name: "key", Type: bigquery.StringFieldType},
-			{Name: "value", Type: bigquery.StringFieldType},
-		}},
-		{Name: "record", Type: bigquery.RecordFieldType, Schema: innerStructSchema},
-		{Name: "record_ptr", Type: bigquery.RecordFieldType, Schema: innerStructSchema},
-		{Name: "record_array", Type: bigquery.RecordFieldType, Schema: innerStructSchema, Repeated: true},
-		{Name: "record_map", Type: bigquery.RecordFieldType, Repeated: true, Schema: bigquery.Schema{
-			{Name: "key", Type: bigquery.StringFieldType},
-			{Name: "value", Type: bigquery.RecordFieldType, Schema: innerStructSchema},
-		}},
-		{Name: "enum", Type: bigquery.StringFieldType},
-		// DeprecatedField doesn't appear in the schema as it has been deprecated
-	}
+	schema := EmbeddedSchema.AssetTable
 	// This ensures that we are testing all the field types that exist in the schema
 	ensureTypeSetCoverage(t, schema)
 
-	obj := &TestStruct{
-		IntScalar:       3,
-		StringScalar:    "foo",
-		BoolScalar:      false,
-		IntArray:        []int{1, 2, 3},
-		Float32Scalar:   0.32,
-		Float64Scalar:   0.64,
-		DynamicMap:      map[string]string{"foo": "bar", "fizz": "buzz"},
-		Timestamp:       *timestamppb.New(time.Unix(1337, 1773)),
-		TimestampPtr:    timestamppb.New(time.Unix(1337, 1773)),
-		Record:          TestInnerStruct{FieldA: "test", FieldB: 10},
-		RecordPtr:       &TestInnerStruct{FieldA: "test", FieldB: 10},
-		NullPtr:         nil,
-		RecordArray:     []*TestInnerStruct{{FieldA: "1", FieldB: 1}, {FieldA: "2", FieldB: 2}},
-		RecordMap:       map[string]*TestInnerStruct{"key": {FieldA: "value"}},
-		DeprecatedField: "I should not be in the result JSON",
-		Enum:            1,
+	asset := migrationcenterpb.Asset{
+		Name:       "foo",                              // string
+		CreateTime: timestamppb.New(time.Unix(10, 10)), // timestamp
+		Labels: map[string]string{ // map
+			"key":  "value",
+			"key2": "value2",
+		},
+		InsightList: &migrationcenterpb.InsightList{
+			Insights: []*migrationcenterpb.Insight{
+				{
+					Insight: &migrationcenterpb.Insight_MigrationInsight{ // one_of
+						MigrationInsight: &migrationcenterpb.MigrationInsight{
+							Fit: &migrationcenterpb.FitDescriptor{
+								FitLevel: migrationcenterpb.FitDescriptor_FIT, // enum
+							},
+						},
+					},
+				},
+			},
+		},
+		AssetDetails: &migrationcenterpb.Asset_MachineDetails{
+			MachineDetails: &migrationcenterpb.MachineDetails{
+				MachineName: "foo",
+				MemoryMb:    10, // integer
+			},
+		},
 	}
 
-	got, err := SerializeObjectToBigQuery(obj, "object", schema)
+	serializer := NewSerializer[*migrationcenterpb.Asset]("asset", schema)
+
+	got, err := serializer(&asset)
 	if err != nil {
-		t.Fatalf("SerializeObjectToBigQuery(%+v, ...): unexpected error: %v", obj, err)
+		t.Fatalf("SerializeObjectToBigQuery(%+v, ...): unexpected error: %v", &asset, err)
 	}
 
 	// We do this so that the diff is more human readable
 	got = prettyPrintJSON(got)
 	if diff := golden.Compare(t, "exporter.json", string(got)); diff != "" {
-		t.Fatalf("SerializeObjectToBigQuery(%+v, ...): mismatch (-want, +got):\n%s", obj, diff)
-	}
-}
-
-func TestInvalidTimestamp(t *testing.T) {
-	type TestStruct struct {
-		Timestamp int
-	}
-	schema := bigquery.Schema{
-		{Name: "timestamp", Type: bigquery.TimestampFieldType},
-	}
-	obj := &TestStruct{
-		Timestamp: 10,
-	}
-
-	got, err := SerializeObjectToBigQuery(obj, "object", schema)
-	if err == nil {
-		t.Fatalf("SerializeObjectToBigQuery(%+v, ...): succeeded unexpectedly", obj)
-	}
-
-	if diff := cmp.Diff([]uint8(nil), got); diff != "" {
-		t.Fatalf("SerializeObjectToBigQuery(%+v, ...): mismatch (-want, +got):\n%s", obj, diff)
+		t.Fatalf("SerializeObjectToBigQuery(%+v, ...): mismatch (-want, +got):\n%s", &asset, diff)
 	}
 }
 
